1218816d9878a1c6ae9e5618fdfe3ec588382ef3
Andy Strassburg <andypro.acct+github@gmail.com>
2011-05-18 20:51:47 +0000

    See if we need the ncurses color command anymore since we're passing vtxxx control sequences directly to the term.

--- src/cwin.c
+++ src/cwin.c
@@ -212,8 +212,8 @@ curses_start()
 		bail(YTE_INIT);
 	}
 
-	if(def_flags & FL_COLOR)
-		start_color();  //  Added by ap - need to start here if we want to have a chance of showing colors
+	//if(def_flags & FL_COLOR)
+	//	start_color();  //  Added by ap - need to start here if we want to have a chance of showing colors
 
 	noraw();
 	crmode();
